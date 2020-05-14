3e718a9edc8ed355a279851023e025ab59ec2a78
Nisha Raichur <nisha.raichur@uni.ulm.de>
2020-05-14 15:42:08 +0200

    Adss the adversarial perturbations in a loop(epsilon values)

--- fgsm.py
+++ fgsm.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from torch.autograd import Variable
 import torch.nn.functional as F
 import numpy as np
-
+from utils import return_class_name, return_class_accuracy, visualize
 
 alexnet = models.alexnet(pretrained=True)
 alexnet.eval()
@@ -36,3 +36,18 @@ loss = torch.nn.CrossEntropyLoss()
 loss_val = loss(predictions, target)
 loss_val.backward(retain_graph = True)
 grads = torch.sign(input_image.grad.data)
+
+list_accu = []
+epsilon = [0.0001, 0.001, 0.0013, 0.009, 0.015, 0.120, 0.25, 0.302]
+for i in epsilon:
+  adversarial_image = input_image.data + i * grads
+  torch.clamp(adversarial_image, min=0, max=1)
+  predictions_adv = alexnet.forward(Variable(adversarial_image))
+
+  adversarial_class, class_index = return_class_name(predictions_adv)
+  adversarial_acc = return_class_accuracy(predictions_adv, class_index)
+
+  acc_of_original = return_class_accuracy(predictions_adv, target_dim)
+
+  visualize(input_image, adversarial_image, i, grads, target_class, adversarial_class, target_acc, adversarial_acc, acc_of_original)
+  list_accu.append(acc_of_original)
